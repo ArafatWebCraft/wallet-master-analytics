cacfb1725f73d5683c7f20555c8c6dcfe1a3d6b3
ArafatWebCraft <arafathossan377@gmail.com>
2024-05-24 16:08:06 +0600

    wallet-master

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var options = {
+    series: [44, 55],
+    chart: {
+        type: 'donut',
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            chart: {
+                width: 200
+            },
+            legend: {
+                position: 'bottom'
+            }
+        }
+    }]
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
